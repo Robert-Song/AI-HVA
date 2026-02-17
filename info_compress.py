fe08c35e65264ec694071ac1b3aebb51facd196b
rileycn <rileycnoggle@gmail.com>
2026-02-17 11:55:52 -0500

    finished compressor, added test cases

--- info_compress.py
+++ info_compress.py
@@ -10,10 +10,22 @@ class InfoCompressor:
         for i in range(len(sch.schematicSymbols)):
             newprop = []
             sch.schematicSymbols[i].properties = newprop
+        for i in range(len(sch.labels)):
+            newprop = []
+            print(sch.labels[i])
+            sch.labels[i].properties = newprop
+        #sch.to_file("result.kicad_sch")
+        return sch.to_sexpr()
 
-        sch.to_file("result.kicad_sch")
-        print(len(sch.schematicSymbols))
+    ''' Non-essential test case
+    def bad_parse(self, filepath):
+        sch = Schematic().from_file(filepath)
+        newsch = Schematic()
+        lb = sch.libSymbols
+        newsch.libSymbols = lb
+        newsch.to_file("result.kicad_sch")
+    '''
 
 
-ic = InfoCompressor()
-ic.parse("example.kicad_sch")
+#ic = InfoCompressor()
+#print(ic.parse("example.kicad_sch"))
