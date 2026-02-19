401047c9d735f467709d8a26a9e04448e929bcda
dixon140 <dixon140@purdue.edu>
2026-02-19 12:37:42 -0500

    clean up

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,28 +1,29 @@
 from tkinter import *
 from PIL import Image, ImageTk
 from tkinter import filedialog
+import shutil
 # Initializes root window
 root = Tk()
+
 # Brings the window into focus
 root.lift()
 root.focus_force()
+
 # Sets window title
 root.title("AI-HVA")
+
 # Create and place a label for upload info
 lbl = Label(root, text = "Upload a KiCad schematic file.")
 lbl.grid(row = 0, column = 0, pady=(25, 0), padx=100)
+
 # Import both images for button (hover and normal)
-image = Image.open("uploadnormal.png")
-resized = image.resize((246, 150))
-normalimg = ImageTk.PhotoImage(resized)
-image = Image.open("uploadhover.png")
-resized = image.resize((246, 150))
-hoverimg = ImageTk.PhotoImage(resized)
+normalimg = PhotoImage(file="uploadnormal.png")
+hoverimg = PhotoImage(file="uploadhover.png")
 
 # Define import file logic
 def import_file():
     # Opens file window and ensures root window remains in focus on file dialog close
-    file_path = filedialog.askopenfilename(title="Select a file", filetypes=[("KiCad Schematics", "*.kicad_sch"), ("All files", "*.*")])
+    file_path = filedialog.askopenfilename(title="Select a file", filetypes=[("KiCad Netlists/Schematics", ["*.net", ".kicad_sch"])])
     root.lift()
     root.focus_force()
     # Prints file path for now
