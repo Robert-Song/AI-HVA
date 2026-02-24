c0636fa2e7b6f18e7385b5fc3ff8fe6f53ec8e9e
rileycn <rileycnoggle@gmail.com>
2026-02-24 07:37:14 -0500

    Finished story

--- info_compress.py
+++ info_compress.py
@@ -1,4 +1,7 @@
 from kiutils.schematic import Schematic
+from kiutils.items.common import Effects
+import subprocess
+from kinparse import parse_netlist
 
 class InfoCompressor:
     def __init__(self):
@@ -12,20 +15,50 @@ class InfoCompressor:
             sch.schematicSymbols[i].properties = newprop
         for i in range(len(sch.labels)):
             newprop = []
-            print(sch.labels[i])
-            sch.labels[i].properties = newprop
+            #sch.labels[i].effects = Effects()
+            #print(sch.labels[i])
+        for i in range(len(sch.hierarchicalLabels)):
+            newprop = []
+            #print(sch.hierarchicalLabels[i])
+            sch.hierarchicalLabels[i].properties = newprop
         #sch.to_file("result.kicad_sch")
         return sch.to_sexpr()
-
-    ''' Non-essential test case
-    def bad_parse(self, filepath):
+    
+    def convert(self, filepath):
         sch = Schematic().from_file(filepath)
-        newsch = Schematic()
-        lb = sch.libSymbols
-        newsch.libSymbols = lb
-        newsch.to_file("result.kicad_sch")
-    '''
+        newl = []
+        banned = ["C", "R", "Fuse", "GND", "PWR_FLAG"]
+        for sl in sch.libSymbols:
+            allgood = True
+            for bv in banned:
+                if sl.entryName == bv:
+                    allgood = False
+                    break
+            if allgood:
+                newl.append(sl)
+        sch.libSymbols = newl
+        for i in range(len(sch.schematicSymbols)):
+            newprop = []
+            sch.schematicSymbols[i].properties = newprop
+        for i in range(len(sch.labels)):
+            newprop = []
+            #sch.labels[i].effects = Effects()
+            #print(sch.labels[i])
+        for i in range(len(sch.hierarchicalLabels)):
+            newprop = []
+            #print(sch.hierarchicalLabels[i])
+            sch.hierarchicalLabels[i].properties = newprop
+        sch.to_file("result.kicad_sch")
+        subprocess.run(["/Applications/KiCad/KiCad.app/Contents/MacOS/kicad-cli", "sch", "export", "netlist", "result.kicad_sch"])
+    
+    def essential_list_netlist(self, filepath):
+        for libb in parse_netlist(filepath).libparts:
+            print(libb.name + ": " + libb.desc + f" ({libb.docs})")
+    
+    def essential_list_kicad(self, filepath):
+        self.convert(filepath)
+        self.essential_list_netlist("result.net")
 
-
+#Test case
 #ic = InfoCompressor()
-#print(ic.parse("example.kicad_sch"))
+#(ic.essential_list_kicad("example.kicad_sch"))
