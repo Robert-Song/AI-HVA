6438ad3b39f00dc61956ca9a9a31a057c57e7aab
rileycn <rileycnoggle@gmail.com>
2026-02-24 11:51:22 -0500

    Story 15 complete

--- manual_folder.py
+++ manual_folder.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv, set_key
-
+import requests
 
 class ManualFolder:
     def __init__(self):
@@ -23,9 +23,56 @@ class ManualFolder:
     
     def get_manual_folder_path(self):
         return os.environ.get("AIHVA_MAN")
+    
+    def get_fallback(self, partname):
+        filename = os.path.join(self.get_manual_folder_path(), partname) + ".pdf"
+        result = None
+        error = None
+        if not os.path.exists(filename):
+            error = f"Fallback file not found in {filename}"
+            return result, error
+        with open(filename, "r") as file:
+            result = file.buffer.read()
+        return result, error
+    
+    def test_find_datasheets(self, partnames, urls):
+        '''
+        74AHC1G04: Single NOT Gate, Low-Voltage CMOS (http://www.ti.com/lit/sg/scyt129e/scyt129e.pdf)
+        74LVC1G332: Single OR 3-Input Gate, Low-Voltage CMOS (http://www.ti.com/lit/sg/scyt129e/scyt129e.pdf)
+        SiP32431DR3: 10 pA, Ultra Low Leakage and Quiescent Current, Load Switch with Reverse Blocking, High Enable, SC-70-6 (http://www.vishay.com.hk/docs/66597/sip32431.pdf)
+        FDN537N: 6.5A Id, 30V Vds,  Single N-Channel Power Trench MOSFET , 23mOhm Ron, -55 to 150 Â°C (https://www.onsemi.com/pub/Collateral/FDN537N-D.pdf) 
+        '''
+        for i in range(min(len(partnames), len(urls))):
+            print(self.test_find_datasheet(partnames[i], urls[i]))
+
+    def test_find_datasheet(self, partname, url):
+        result = None
+        error = None
+        response = None
+        try:
+            response = requests.get(url)
+        except:
+            pass
+        if response is None or response.status_code != 200:
+            if response:
+                print(f"URL request failed for {partname} at {url}: {response.status_code} {response.reason}")
+            else:
+                print(f"URL request failed for {partname} at {url} for unknown reasons")
+            fresult, ferror = self.get_fallback(partname)
+            if ferror:
+                print(ferror)
+                error = f"URL and fallback failed"
+                return result, error
+            else:
+                return fresult, error
+        result = response.text
+        return result, error
+
 
 mf = ManualFolder()
 error = mf.set_manual_folder_path("Test")
 if error:
     print(error)
 print(f"Current manual folder: {mf.get_manual_folder_path()}")
+mf.test_find_datasheets(["74AHC1G04", "74LVC1G332", "SiP32431DR3", "FDN537N"],
+                        ["http://www.ti.com/lit/sg/scyt129e/scyt129e.pdf", "http://www.ti.com/lit/sg/scyt129e/scyt129e.pdf", "http://www.vishay.com.hk/docs/66597/sip32431.pdf", "https://www.onsemi.com/pub/Collateral/FDN537N-D.pdf"])
